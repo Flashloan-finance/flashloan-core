f2035585e70fbc2d1e017f4e7cce2dd812fb0e8b
Flashloan-finance <123256235+Flashloan-finance@users.noreply.github.com>
2023-02-05 20:28:16 +0100

    Update LendingPool.sol

--- LendingPool.sol
+++ LendingPool.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.5.0;
 
-interface ILendingPool {
+interface LendingPool {
     function flashLoan ( address _receiver, address _reserve, uint256 _amount, bytes calldata _params ) external;
 }
